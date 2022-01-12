2463464cac85c5c88b1b0a69931cfd080cb1b1c4
Rebeca Iulia Libotean <Libotean.Io.Rebeca@student.utcluj.ro>
2022-01-12 19:16:10 +0200

    .

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using AutoGeistModel;
 
 
 namespace Ghirbomean_Rebeca_LAB5
@@ -19,11 +22,36 @@ namespace Ghirbomean_Rebeca_LAB5
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    enum ActionState { New, Edit, Delete, Nothing }
     public partial class MainWindow : Window
     {
+
+        ActionState action = ActionState.Nothing;
+        //using AutoGeistModel;
+        AutoGeistEntitiesModel ctx = new AutoGeistEntitiesModel();
+        CollectionViewSource carViewSource;
+        CollectionViewSource customerViewSource;
+        CollectionViewSource carOrdersViewSource;
+
+        Binding bodyStyleTextBoxBinding = new Binding();
+        Binding modelTextBoxBinding = new Binding();
+        Binding makeTextBoxBinding = new Binding();
+
+        Binding firstNameTextBoxBinding = new Binding();
+        Binding lastNameTextBoxBinding = new Binding();
+        Binding purchaseDateDatePickerTextBoxBinding = new Binding();
+
+
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = this;
+            modelTextBoxBinding.Path = new PropertyPath("Model");
+            makeTextBoxBinding.Path = new PropertyPath("Make");
+            bodyStyleTextBoxBinding.Path = new PropertyPath("BodyStyle");
+            modelTextBox.SetBinding(TextBox.TextProperty, modelTextBoxBinding);
+            makeTextBox.SetBinding(TextBox.TextProperty, makeTextBoxBinding);
+            bodyStyleTextBox.SetBinding(TextBox.TextProperty, bodyStyleTextBoxBinding);
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
@@ -35,6 +63,378 @@ namespace Ghirbomean_Rebeca_LAB5
             System.Windows.Data.CollectionViewSource customerViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("customerViewSource")));
             // Load data by setting the CollectionViewSource.Source property:
             // customerViewSource.Source = [generic data source]
+            carViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("carViewSource")));
+            carViewSource.Source = ctx.Car.Local;
+            ctx.Car.Load();
+
+            carOrdersViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("carOrdersViewSource")));
+            //carOrdersViewSource.Source = ctx.Order.Local;
+            ctx.Order.Load();
+            BindDataGrid();
+            cbCars.ItemsSource = ctx.Car.Local;
+            //cbCars.DisplayMemberPath = "Make";
+            cbCars.SelectedValuePath = "CarId";
+            cbCustomers.ItemsSource = ctx.Customer.Local;
+            //cbCustomers.DisplayMemberPath = "FirstName";
+            cbCustomers.SelectedValuePath = "CustId";
+
         }
+
+        private void btnNew_Click(object sender, RoutedEventArgs e)
+        {
+            action = ActionState.New;
+            TabItem ti = tbCtrlAutoGeist.SelectedItem as TabItem;
+            switch (ti.Header)
+            {
+                case "Cars":
+                    BindingOperations.ClearBinding(bodyStyleTextBox, TextBox.TextProperty);
+                    BindingOperations.ClearBinding(makeTextBox, TextBox.TextProperty);
+                    BindingOperations.ClearBinding(modelTextBox, TextBox.TextProperty);
+                    bodyStyleTextBox.Text = "";
+                    makeTextBox.Text = "";
+                    modelTextBox.Text = "";
+                    Keyboard.Focus(bodyStyleTextBox);
+                    break;
+                case "Customers":
+                    BindingOperations.ClearBinding(firstNameTextBox, TextBox.TextProperty);
+                    BindingOperations.ClearBinding(lastNameTextBox, TextBox.TextProperty);
+                    firstNameTextBox.Text = "";
+                    lastNameTextBox.Text = "";
+                    Keyboard.Focus(firstNameTextBox);
+                    SetValidationBinding();
+                    break;
+                case "Orders":
+                    break;
+            }
+        }
+
+        private void btnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            action = ActionState.Edit;
+            SetValidationBinding();
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            action = ActionState.Delete;
+        }
+
+        private void bntPrevious_Click(object sender, RoutedEventArgs e)
+        {
+            carViewSource.View.MoveCurrentToPrevious();
+        }
+
+        private void btnNext_Click(object sender, RoutedEventArgs e)
+        {
+            carViewSource.View.MoveCurrentToNext();
+
+        }
+
+        private void SaveCar()
+        {
+            Car car = null;
+            if (action == ActionState.New)
+            {
+                try
+                {
+                    car = new Car()
+                    {
+                        Make = makeTextBox.Text.Trim(),
+                        Model = modelTextBox.Text.Trim(),
+                        BodyStyle = bodyStyleTextBox.Text.Trim(),
+                    };
+                    ctx.Car.Add(car);
+                    carViewSource.View.Refresh();
+                    carViewSource.View.MoveCurrentTo(car);
+                    ctx.SaveChanges();
+                }
+                // using System.Data;
+                catch (DataException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                makeTextBox.SetBinding(TextBox.TextProperty, makeTextBoxBinding);
+                modelTextBox.SetBinding(TextBox.TextProperty, modelTextBoxBinding);
+                bodyStyleTextBox.SetBinding(TextBox.TextProperty,
+               bodyStyleTextBoxBinding);
+            }
+            else if (action == ActionState.Edit)
+            {
+                try
+                {
+                    car = (Car)carDataGrid.SelectedItem;
+                    car.Make = makeTextBox.Text.Trim();
+                    car.Model = modelTextBox.Text.Trim();
+                    car.BodyStyle = bodyStyleTextBox.Text.Trim();
+                    ctx.SaveChanges();
+                }
+                catch (DataException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                carViewSource.View.Refresh();
+                carViewSource.View.MoveCurrentTo(car);
+                makeTextBox.SetBinding(TextBox.TextProperty, makeTextBoxBinding);
+                modelTextBox.SetBinding(TextBox.TextProperty, modelTextBoxBinding);
+                bodyStyleTextBox.SetBinding(TextBox.TextProperty,
+               bodyStyleTextBoxBinding);
+            }
+            else if (action == ActionState.Delete)
+            {
+                try
+                {
+                    car = (Car)carDataGrid.SelectedItem;
+                    ctx.Car.Remove(car);
+                    ctx.SaveChanges();
+                }
+                catch (DataException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                carViewSource.View.Refresh();
+                makeTextBox.SetBinding(TextBox.TextProperty, makeTextBoxBinding);
+                modelTextBox.SetBinding(TextBox.TextProperty, modelTextBoxBinding);
+                bodyStyleTextBox.SetBinding(TextBox.TextProperty,
+               bodyStyleTextBoxBinding);
+            }
+        }
+
+        private void btnPrev1_Click(object sender, RoutedEventArgs e)
+        {
+            carViewSource.View.MoveCurrentToPrevious();
+        }
+
+        private void btnNext1_Click(object sender, RoutedEventArgs e)
+        {
+            carViewSource.View.MoveCurrentToNext();
+
+        }
+
+        private void SaveCustomers()
+        {
+            Customer customer = null;
+            if (action == ActionState.New)
+            {
+                try
+                {
+                    customer = new Customer()
+                    {
+                        FirstName = firstNameTextBox.Text.Trim(),
+                        LastName = lastNameTextBox.Text.Trim(),
+                        PurchaseDate = purchaseDateDatePicker.SelectedDate
+                    };
+                    ctx.Customer.Add(customer);
+                    customerViewSource.View.Refresh();
+                    customerViewSource.View.MoveCurrentTo(customer);
+                    ctx.SaveChanges();
+                }
+                // using System.Data;
+                catch (DataException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                firstNameTextBox.SetBinding(TextBox.TextProperty, firstNameTextBoxBinding);
+                lastNameTextBox.SetBinding(TextBox.TextProperty, lastNameTextBoxBinding);
+                purchaseDateDatePicker.SetBinding(TextBox.TextProperty, purchaseDateDatePickerTextBoxBinding);
+            }
+            else if (action == ActionState.Edit)
+            {
+                try
+                {
+                    customer = (Customer)customerDataGrid.SelectedItem;
+                    customer.FirstName = firstNameTextBox.Text.Trim();
+                    customer.LastName = lastNameTextBox.Text.Trim();
+                    customer.PurchaseDate = purchaseDateDatePicker.SelectedDate;
+                    ctx.SaveChanges();
+                }
+                catch (DataException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                customerViewSource.View.Refresh();
+                customerViewSource.View.MoveCurrentTo(customer);
+                firstNameTextBox.SetBinding(TextBox.TextProperty, firstNameTextBoxBinding);
+                lastNameTextBox.SetBinding(TextBox.TextProperty, lastNameTextBoxBinding);
+                purchaseDateDatePicker.SetBinding(TextBox.TextProperty, purchaseDateDatePickerTextBoxBinding);
+            }
+            else if (action == ActionState.Delete)
+            {
+                try
+                {
+                    customer = (Customer)customerDataGrid.SelectedItem;
+                    ctx.Customer.Remove(customer);
+                    ctx.SaveChanges();
+                }
+                catch (DataException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                customerViewSource.View.Refresh();
+                firstNameTextBox.SetBinding(TextBox.TextProperty, firstNameTextBoxBinding);
+                lastNameTextBox.SetBinding(TextBox.TextProperty, lastNameTextBoxBinding);
+                purchaseDateDatePicker.SetBinding(TextBox.TextProperty, purchaseDateDatePickerTextBoxBinding);
+            }
+        }
+        private void gbOperations_Click(object sender, RoutedEventArgs e)
+        {
+            Button SelectedButton = (Button)e.OriginalSource;
+            Panel panel = (Panel)SelectedButton.Parent;
+            foreach (Button B in panel.Children.OfType<Button>())
+            {
+                if (B != SelectedButton)
+                    B.IsEnabled = false;
+            }
+            gbActions.IsEnabled = true;
+        }
+
+        private void ReInitialize()
+        {
+            Panel panel = gbOperations.Content as Panel;
+            foreach (Button B in panel.Children.OfType<Button>())
+            {
+                B.IsEnabled = true;
+            }
+            gbActions.IsEnabled = false;
+        }
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            ReInitialize();
+        }
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+
+            TabItem ti = tbCtrlAutoGeist.SelectedItem as TabItem;
+            switch (ti.Header)
+            {
+                case "Cars":
+                    SaveCar();
+                    break;
+                case "Customers":
+                    SaveCustomers();
+                    break;
+                case "Orders":
+                    break;
+            }
+            ReInitialize();
+        }
+
+        private void SaveOrders()
+        {
+            Order order = null;
+            if (action == ActionState.New)
+            {
+                try
+                {
+                    Car car = (Car)cbCars.SelectedItem;
+                    Customer customer = (Customer)cbCustomers.SelectedItem;
+                    //instantiem Order entity
+                    order = new Order()
+                    {
+                        CarId = car.CarId,
+                        CustId = customer.CustId
+                    };
+                    //adaugam entitatea nou creata in context
+                    ctx.Order.Add(order);
+                    ctx.SaveChanges();
+                    BindDataGrid();
+                }
+                catch (DataException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+            else if (action == ActionState.Edit)
+            {
+                dynamic selectedOrder = orderDataGrid.SelectedItem;
+                try
+                {
+                    int curr_id = selectedOrder.OrderId;
+                    var editedOrder = ctx.Order.FirstOrDefault(s => s.OrderId ==
+                    curr_id);
+                    if (editedOrder != null)
+                    {
+                        editedOrder.CarId =
+                       Convert.ToInt32(cbCars.SelectedValue.ToString());
+                        editedOrder.CustId =
+                       Int32.Parse(cbCustomers.SelectedValue.ToString());
+                        //salvam modificarile
+                        ctx.SaveChanges();
+                    }
+                }
+                catch (DataException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                BindDataGrid();
+                // pozitionarea pe item-ul curent
+                carOrdersViewSource.View.MoveCurrentTo(selectedOrder);
+            }
+            else if (action == ActionState.Delete)
+            {
+                try
+                {
+                    dynamic selectedOrder = orderDataGrid.SelectedItem;
+                    int curr_id = selectedOrder.OrderId;
+                    var deletedOrder = ctx.Order.FirstOrDefault(s => s.OrderId ==
+                   curr_id);
+                    if (deletedOrder != null)
+                    {
+                        ctx.Order.Remove(deletedOrder);
+                        ctx.SaveChanges();
+                        MessageBox.Show("Order Deleted Successfully", "Message");
+                        BindDataGrid();
+                    }
+                }
+                catch (DataException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+        private void BindDataGrid()
+        {
+            var queryOrder = from ord in ctx.Order
+                             join cust in ctx.Customer on ord.CustId equals cust.CustId
+                             join car in ctx.Car on ord.CarId equals car.CarId
+                             select new
+                             {
+                                 ord.OrderId,
+                                 ord.CarId,
+                                 ord.CustId,
+                                 cust.FirstName,
+                                 cust.LastName,
+                                 car.Make,
+                                 car.Model
+                             };
+            carOrdersViewSource.Source = queryOrder.ToList();
+        }
+
+        private void orderDataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void SetValidationBinding()
+        {
+            Binding firstNameValidationBinding = new Binding();
+            firstNameValidationBinding.Source = customerViewSource;
+            firstNameValidationBinding.Path = new PropertyPath("FirstName");
+            firstNameValidationBinding.NotifyOnValidationError = true;
+            firstNameValidationBinding.Mode = BindingMode.TwoWay;
+            firstNameValidationBinding.UpdateSourceTrigger =
+           UpdateSourceTrigger.PropertyChanged;
+            //string required
+            firstNameValidationBinding.ValidationRules.Add(new StringNotEmpty());
+            firstNameTextBox.SetBinding(TextBox.TextProperty, firstNameValidationBinding);
+            Binding lastNameValidationBinding = new Binding();
+            lastNameValidationBinding.Source = customerViewSource;
+            lastNameValidationBinding.Path = new PropertyPath("LastName");
+            lastNameValidationBinding.NotifyOnValidationError = true;
+            lastNameValidationBinding.Mode = BindingMode.TwoWay;
+            lastNameValidationBinding.UpdateSourceTrigger =UpdateSourceTrigger.PropertyChanged;
+            //string min length validator
+            lastNameValidationBinding.ValidationRules.Add(new StringMinLength());
+            lastNameTextBox.SetBinding(TextBox.TextProperty, lastNameValidationBinding); //setare binding nou
+}
     }
 }
